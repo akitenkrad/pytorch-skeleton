e6cc861e9e7b348b2ebf3a592e7db58c5fe896a2
akitenkrad <akitenkrad@gmail.com>
2021-08-09 17:22:07 +0900

    update train.py

--- train.py
+++ train.py
@@ -20,19 +20,16 @@ def validate(epoch, valid_dl:DataLoader, model, loss_func, threshold):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     loss_watcher = SimpleWatcher('loss', default_value=sys.maxsize)
-    auc_watcher = AucWatcher('auc', threshold=threshold)
 
     with tqdm(valid_dl, total=len(valid_dl), desc=f'[Epoch {epoch:4d} - Validate]', leave=False) as valid_it:
         for x, y in valid_it:
-            x, y = x.type(torch.float32).to(device), y.type(torch.float32).to(device)
+            x, y = x.type(torch.float32).to(device), y.type(torch.long).to(device)
             with torch.no_grad():
                 out = model(x)
-                out, y = out.squeeze(), y.squeeze()
                 loss = loss_func(out, y)
 
                 loss_watcher.put(loss.item())
-                auc_watcher.put(out.cpu().numpy(), y.cpu().numpy())
-    return auc_watcher.auc, loss_watcher.mean()
+    return loss_watcher.mean()
 
 def save_model(config, model, name):
     save_dir = Path(config.weights_dir)
@@ -61,7 +58,7 @@ def train(config:AttrDict, dataset:BaseDataset, model:nn.Module, optimizer:optim
 
                 # Epoch roop
                 with tqdm(range(config.epochs), total=config.epochs, desc=f'[Fold {fold:2d} / Epoch   0]', leave=False) as epoch_it:
-                    auc_watcher = SimpleWatcher('auc', default_value=-1, patience=config.early_stop_patience)
+                    valid_loss_watcher = SimpleWatcher('valid_loss', default_value=-1, patience=config.early_stop_patience)
 
                     for epoch in epoch_it:
                         loss_watcher = SimpleWatcher('loss', default_value=sys.maxsize)
@@ -89,35 +86,34 @@ def train(config:AttrDict, dataset:BaseDataset, model:nn.Module, optimizer:optim
                                 batch_it.set_description(f'[Epoch {epoch:3d} / Batch {batch:3d}] Loss: {loss.item():.5f}')
 
                         # evaluation
-                        val_auc, val_loss = validate(epoch, valid_dl, model, loss_func, config.threshold)
+                        val_loss = validate(epoch, valid_dl, model, loss_func, config.threshold)
 
                         # step scheduler
                         lr_scheduler.step()
 
                         # update iteration description
-                        desc = f'[Fold {fold:2d} / Epoch {epoch:3d}] Train Loss: {loss_watcher.mean():.5f} | Valid Loss:{val_loss:.5f} | Valid AUC: {val_auc:.5f}'
+                        desc = f'[Fold {fold:2d} / Epoch {epoch:3d}] Train Loss: {loss_watcher.mean():.5f} | Valid Loss:{val_loss:.5f}'
                         epoch_it.set_description(desc)
 
                         # logging
                         last_lr = lr_scheduler.get_last_lr()[0]
-                        logger.info(f'[Fold {fold:2d} / Epoch {epoch:3d}] Train Loss: {loss_watcher.mean():.5f} | Valid Loss:{val_loss:.5f} | Valid AUC: {val_auc:.5f} | LR: {last_lr:.7f}')
+                        logger.info(f'[Fold {fold:2d} / Epoch {epoch:3d}] Train Loss: {loss_watcher.mean():.5f} | Valid Loss:{val_loss:.5f} | LR: {last_lr:.7f}')
                         writer.add_text('train_log', desc, epoch)
                         writer.add_scalar('train loss', loss_watcher.mean(), epoch)
                         writer.add_scalar('valid loss', val_loss, epoch)
-                        writer.add_scalar('valid auc', val_auc, epoch)
 
                         # save best model
-                        if auc_watcher.is_best:
+                        if valid_loss_watcher.is_best:
                             save_model(config, model, f'{config.model}_best.pt')
-                        auc_watcher.put(val_auc)
+                        valid_loss_watcher.put(val_loss)
 
                         # save model regularly
                         if epoch % 5 == 0:
                             save_model(config, model, f'{config.model}_last.pt')
 
                         # early stopping
-                        if auc_watcher.early_stop:
-                            logger.info(f'====== Early Stopping @epoch: {epoch} @AUC: {auc_watcher.best_score:5.10f} ======')
+                        if valid_loss_watcher.early_stop:
+                            logger.info(f'====== Early Stopping @epoch: {epoch} @AUC: {valid_loss_watcher.best_score:5.10f} ======')
                             save_model(config, model, f'{config.model}_last.pt')
                             break
 
